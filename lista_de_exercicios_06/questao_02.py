866ab7aeb290f3b31a901ebd8231cdecd2b4ae93
rodrigo <rodrigosc1025@gmail.com>
2024-09-17 15:41:08 -0300

    .

--- lista_de_exercicios_06/questao_02.py
+++ lista_de_exercicios_06/questao_02.py
@@ -1,5 +1,23 @@
-from sys import argv
+def calculaTabuada(numero):
+    resultado = ""
+    for i in range(1, 11):
+        
+        print(f'{numero} X {i} = {numero * i}')
+        resultado = resultado + (f'{numero} X {i} = {numero * i}\n')
+    return resultado
 
-for i in range(0 , 11):
-    for j in range(0, 11):
-        print(f'{i} X {j} = {i * j}')
+#Recebe um arquivo txt e imprime nele
+def salvaEmArquivo(arquivo, conteudo):
+    with open(arquivo, 'w') as arquivo:
+        arquivo.write(conteudo)
+
+
+if __name__ == '__main__':
+    #numero = int(input('Digite um numero para calcular a tabuada: '))
+    tabuada = ""
+    for i in range (1, 11):
+        tabuada = tabuada + calculaTabuada(i)
+        
+    
+    arquivo = 'tabuada2.txt' #Quando executado cria um arquivo com o nome tabuada.txt
+    salvaEmArquivo(arquivo, tabuada)
