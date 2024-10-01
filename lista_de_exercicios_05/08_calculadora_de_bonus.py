a93a6d28d75cee4950245af426ea27dcdc47fa89
rodrigo <rodrigosc1025@gmail.com>
2024-10-01 00:34:30 -0300

    Update files

--- lista_de_exercicios_05/08_calculadora_de_bonus.py
+++ lista_de_exercicios_05/08_calculadora_de_bonus.py
@@ -1,3 +1,7 @@
+# Calculadora de Bônus 
+# a. Descrição: Um rei deseja distribuir bônus aos seus cavaleiros com base em suas conquistas. Se um cavaleiro completou mais de 10 missões, ele recebe um bônus de 100 moedas de ouro. Se completou entre 5 e 10 missões, recebe 50 moedas de ouro. Se completou menos de 5, recebe 10 moedas de ouro. Crie um programa que receba o número de missões completadas e informe o valor do bônus. 
+# b. Conceito: Desvio condicional aninhado, operadores relacionais. 
+
 def bonus(numeroMissoes):
     
     if numeroMissoes > 10:
--- lista_de_exercicios_05/09_escolha_de_feiticos.py
+++ lista_de_exercicios_05/09_escolha_de_feiticos.py
@@ -1,3 +1,7 @@
+# Escolha de Feitiços 
+# a. Descrição: Um mago tem três feitiços: fogo, água e terra. Crie um programa que receba a escolha do usuário (1 para fogo, 2 para água, 3 para terra) e use o comando match-case para exibir o feitiço escolhido. 
+# b. Conceito: Match-case, variáveis. 
+
 def castFeitico(escolha):
     match escolha:
         case 1:
