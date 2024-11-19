652f0290869f1eb1761fbba040f827377be2c548
rodrigosantoscosta <rodrigosc1025@gmail.com>
2024-11-18 21:16:55 -0300

    API Advices

--- api_advices_test.py
+++ api_advices_test.py
@@ -1,11 +1,19 @@
 import requests
+from loguru import logger
+arquivo = []
 
 r = requests.get('https://api.adviceslip.com/advice')
+try:
+    print(r)
+    print(r.status_code)
+    print(r.text)
+    print(r.json())
+    print(r.json()['slip'])
+    print(r.json()['slip']['id'])
+    print(r.json()['slip']['advice'])
+except Exception as error:
+    logger.exception(f'Error: {error}')
 
-print(r)
-print(r.status_code)
-print(r.text)
-print(r.json())
-print(r.json()['slip'])
-print(r.json()['slip']['id'])
-print(r.json()['slip']['advice'])
+with open('arquivo.txt', 'w', encoding='UTF-8') as arquivo:
+    arquivo.write(r.text)
+
